affd708ac230f7cccde1c783b4da2594e3dd0b75
Mário Feroldi <mferoldif@gmail.com>
2019-04-17 16:19:40 -0300

    Separate filters by channel

--- example.py
+++ example.py
@@ -4,6 +4,12 @@ import giraffe as gf
 import numpy as np
 import matplotlib.pyplot as plt
 
+image = gf.load_image('lenna_50.png')
+
+out = gf.sobel_filter(image)
+
+gf.save_image(out, 'out.png')
+
 def plot_histogram(image):
     r, g, b = gf.histogram(image)
     fig, (axr, axg, axb) = plt.subplots(3)
@@ -11,11 +17,3 @@ def plot_histogram(image):
     axg.bar(g.keys(), g.values(), color='green')
     axb.bar(b.keys(), b.values(), color='blue')
     plt.show()
-
-image = gf.load_image('lenna.png')
-
-out = gf.mode_filter(image)
-
-gf.save_image(out, 'out.png')
-
-plot_histogram(out)
--- giraffe.py
+++ giraffe.py
@@ -41,30 +41,44 @@ def reverse(image: np.ndarray) -> np.ndarray:
 
     return _transform(f, image)
 
-# TODO: change this to work on only (w,h) instead of (w,h,3)
 
-def nonlinear_filter(image: np.ndarray, transform, window_shape=(3, 3)) -> np.ndarray:
+def nonlinear_filter_channel(
+    channel: np.ndarray, transform, window_shape=(3, 3)
+) -> np.ndarray:
     window_w, window_h = window_shape
-    image_w, image_h, image_depth = image.shape
-    image_padded = _pad_matrix(matrix=image, width=window_w // 2, height=window_h // 2)
-    output_image = np.zeros(image.shape, dtype=image.dtype)
+    image_w, image_h = channel.shape
+    image_padded = _pad_matrix(
+        matrix=channel, width=window_w // 2, height=window_h // 2
+    )
+    output_image = np.zeros(channel.shape, dtype=channel.dtype)
     for i in range(image_w):
         for j in range(image_h):
             neighbors = _neighbors(i, j, *window_shape, image_padded)
-            pixel = [transform(neighbors[..., d]) for d in range(image_depth)]
+            pixel = transform(neighbors)
             output_image[i, j] = pixel
     return output_image
 
-def correlate(image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
-    def g(neighbors):
-        pixel = np.sum(neighbors * kernel)
-        return _clamp_pixel(pixel)
 
-    return nonlinear_filter(image, g, window_shape=kernel.shape)
+def nonlinear_filter(image: np.ndarray, transform, window_shape=(3, 3)) -> np.ndarray:
+    (red, green, blue) = split_channels(image)
+    red = nonlinear_filter_channel(red, transform, window_shape=window_shape)
+    green = nonlinear_filter_channel(green, transform, window_shape=window_shape)
+    blue = nonlinear_filter_channel(blue, transform, window_shape=window_shape)
+    return np.dstack((red, green, blue))
+
+
+def correlate(image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
+    normal_kernel = normalize_kernel(kernel)
+
+    def apply_kernel(neighbors):
+        pixel = np.sum(neighbors * normal_kernel)
+        return np.clip(pixel, 0, 255)
+
+    return nonlinear_filter(image, apply_kernel, window_shape=normal_kernel.shape)
 
 
 def convolve(image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
-    return correlate(image, convolve_kernel(kernel))
+    return correlate(image, kernel[::-1, ::-1])
 
 
 def mean_filter(image: np.ndarray, window_shape=(3, 3)) -> np.ndarray:
@@ -89,20 +103,24 @@ def mode_filter(image: np.ndarray, window_shape=(3, 3)) -> np.ndarray:
 
 
 def sobel_filter(image: np.ndarray) -> np.ndarray:
-    sobel_x = convolve_kernel(np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]))
-    sobel_y = convolve_kernel(np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]]))
+    sobel_x = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+    sobel_y = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
 
-    def sobel(neighbors):
-        shape = neighbors.shape
-        sx = np.sum(neighbors * sobel_x)
-        sy = np.sum(neighbors * sobel_y)
-        return np.sqrt(sx * sx + sy * sy).astype(np.uint8)
+    conv_x = convolve(image, sobel_x)
+    conv_y = convolve(image, sobel_y)
 
-    return nonlinear_filter(image, sobel, window_shape=(sobel_x.shape[0],sobel_y.shape[1]))
+    r_x, g_x, b_x = split_channels(conv_x)
+    r_y, g_y, b_y = split_channels(conv_y)
+
+    r = np.sqrt(r_x * r_x + r_y * r_y)
+    g = np.sqrt(g_x * g_x + g_y * g_y)
+    b = np.sqrt(b_x * b_x + b_y * b_y)
+
+    return np.dstack((r, g, b)).astype(np.uint8)
 
 
 def smoothen(image: np.ndarray) -> np.ndarray:
-    kernel = normalize_kernel(np.array([1, 1, 1, 1, 1, 1, 1, 1, 1]).reshape(3, 3))
+    kernel = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1]).reshape(3, 3)
     return convolve(image, kernel)
 
 
@@ -127,7 +145,9 @@ def intensified_sharpen(image: np.ndarray) -> np.ndarray:
 
 
 def normalize_kernel(kernel: np.ndarray) -> np.ndarray:
-    return kernel / np.sum(kernel)
+    kernel_sum = np.sum(kernel)
+    divider = kernel_sum if kernel_sum != 0 else 1
+    return kernel / divider
 
 
 def histogram(image: np.ndarray):
@@ -147,35 +167,21 @@ def channel_histogram(channel: np.ndarray):
 
 def split_channels(image: np.ndarray):
     r_chan, g_chan, b_chan = np.dsplit(image, image.shape[-1])
-    return (
-        r_chan[..., 0],
-        g_chan[..., 0],
-        b_chan[..., 0],
-    )
+    return (r_chan[..., 0], g_chan[..., 0], b_chan[..., 0])
 
 
-def convolve_kernel(kernel: np.ndarray) -> np.ndarray:
+def rotate180(kernel: np.ndarray) -> np.ndarray:
     return np.rot90(kernel, k=2)
 
+
 def _pad_matrix(matrix: np.ndarray, width, height) -> np.ndarray:
-    pads = ((width, width), (height, height), (0, 0))
+    pads = ((width, width), (height, height))
     return np.pad(matrix, pad_width=pads, mode="symmetric")
 
 
 def _neighbors(i, j, w, h, matrix):
     indexes = np.ix_(range(i, i + w), range(j, j + h))
     return matrix[indexes]
-
-
-def _clamp(value, low, high):
-    return max(low, min(high, value))
-
-def _clamp_pixel(pixel):
-    r, g, b = pixel
-    r = _clamp(r, 0, 255)
-    g = _clamp(g, 0, 255)
-    b = _clamp(b, 0, 255)
-    return [r, g, b]
 
 
 def _transform(f, input_matrix: np.ndarray) -> np.ndarray:
